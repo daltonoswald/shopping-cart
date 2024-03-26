b269dc008a56d7766e1bd21938d605f86b75d7f8
daltonoswald <daltonoswald@gmail.com>
2024-03-26 13:09:32 -0400

    added delete on cart page

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,27 +1,29 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Nav from "./Nav"
 
 function Cart({itemsInCart, setItemsInCart}) {
-    console.log(itemsInCart);
     return (
         <>
             <Nav itemsInCart={itemsInCart} />
             <div className="content">
                 <div className="cart-container">
                     <div className="cart-heading">Cart</div>
-                    {/* {itemsInCart.map((item) =>
-                            <div>{item}</div>
-                    )} */}
                     <div className="items-in-cart">
                     {itemsInCart.length !== 0 ? (
                         itemsInCart.map(item => (
-                            <CartItem key={item[0].id} data={item} />
+                            <CartItem itemsInCart={itemsInCart} setItemsInCart={setItemsInCart} key={item[0].id} id={item[0].id} data={item} />
                         ))
                     )
                     : (
-                            <div className="item-wrapper">Nothing in your cart.</div>
+                        <>
+                            <div className="empty-cart">There are no items in your cart. 
+                            <Link className="continue-shopping" to="/store">Continue Shopping</Link> 
+                            </div>
+                        </>
+                            
                     )}
                     </div>
                 </div>
@@ -31,7 +33,7 @@ function Cart({itemsInCart, setItemsInCart}) {
     )
 }
 
-function CartItem({ data }) {
+function CartItem({ data, itemsInCart, setItemsInCart }) {
     const [quantity, setQuantity] = useState(data[1]);
 
     function handleDecrease() {
@@ -40,8 +42,13 @@ function CartItem({ data }) {
         }
     }
 
+    function removeItem(data) {
+        const updatedCart = itemsInCart.filter(item => item[0].id !== data[0].id);
+        setItemsInCart(updatedCart);
+    }
+
     return (
-        <div className="item-wrapper">
+        <div key={data[0].id} id={data[0].id} className="item-wrapper">
             <img src={data[0].image} className="item-image" />
             <div className="item-card">
                 <h3>{data[0].title}</h3>
@@ -50,8 +57,10 @@ function CartItem({ data }) {
                     <div className="quantity">{quantity}</div>
                     <button id="plus" onClick={() => setQuantity((quantity) => quantity +1)}>+</button>
                 </div>
+                {/* <button className="remove" onClick={removeItem}>Remove</button> */}
                 <div className="item-price">${(data[0].price * (quantity)).toFixed(2)}</div>
             </div>
+            <button className="remove" onClick={()=> removeItem(data)}>Remove</button>
         </div>
     )
 }
@@ -64,7 +73,6 @@ function Total({ itemsInCart }) {
             let price = 0;
             for(let i = 0; i < itemsInCart.length; i++) {
                 price += itemsInCart[i][0].price * itemsInCart[i][1];
-                // price = price.toFixed(2);
             }
             let fixedPrice = price.toFixed(2);
             setTotalPrice(fixedPrice);
@@ -73,7 +81,10 @@ function Total({ itemsInCart }) {
     },[itemsInCart])
 
     return (
-        <div className="cart-total">Total: ${totalPrice}</div>
+        <div className="total-container">
+            <div className="cart-total">Total: ${totalPrice}</div>
+            <button className="checkout">Checkout</button>
+        </div>
     )
 }
 
